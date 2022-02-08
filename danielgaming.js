c28484f115171ae56b840a4a6cc947babee76454
AkkoFortySeven <snufflesthesnowballseahorse@gmail.com>
2022-02-08 18:00:48 +1300

    2

--- danielgaming.js
+++ danielgaming.js
@@ -5,7 +5,8 @@ require("dotenv").config()
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -17,6 +18,12 @@ client.on("messageCreate", (message) => {
     if (message.content == "hi"){
         message.reply("Fuck you Ed Sheeran")
     }
+})
+
+const welcomingChannelID = "200120357095604225"
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomingChannelID).send(`<@${member.id}> Who the fuck are you`)
 })
 
 client.login(process.env.TOKEN)
